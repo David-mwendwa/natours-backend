85d2ee6eae0f450b1a3febd906bf7369a6dfd03b
David Mwendwa <klatzdavie19@gmail.com>
2021-09-12 19:39:44 +0300

    express setup

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('natours app')
+const express = require('express');
+
+app.get('/', (req, res) => {
+  res.send('Home page')
+})
+
+app.listen(5000, () => {
+  console.log('App listening on port 5000')
+})
