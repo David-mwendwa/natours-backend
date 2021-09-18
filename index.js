232353f7b8bfe292f8fb10a2c2dba3591e88d66b
David Mwendwa <klatzdavie19@gmail.com>
2021-09-18 21:09:42 +0300

    c

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.get('/api/v1/tours/:id', (req, res) => {
 });
 
 app.post('/api/v1/tours', (req, res) => {
-  //console.log(req.body)
+  console.log(req.body)
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({id: newId}, req.body);
 
