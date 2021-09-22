04628cafab361d952434b124e4f00d1ee39e3918
David Mwendwa <klatzdavie19@gmail.com>
2021-09-22 20:35:59 +0300

    feat: chain multiple middleware functions - tour - post

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -15,6 +15,16 @@ exports.checkID = (req, res, next, val) => {
   next();
 };
 
+exports.checkBody = (req, res, next) => {
+  const { name, price } = req.body;
+  if (!name || !price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price',
+    });
+  }
+};
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
