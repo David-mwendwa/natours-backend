c3901abe7513f4bc4034ee174d5e109998deec3a
David Mwendwa <klatzdavie19@gmail.com>
2021-10-14 17:24:48 +0300

    _checkID_ undo

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,16 +4,16 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
-// exports.checkID = (req, res, next, val) => {
-//   //console.log(`Tour id is: ${val}`);
-//   if (+req.params.id > tours.length) {
-//     return res.status(404).json({
-//       status: 'fail',
-//       message: 'Invalid ID',
-//     });
-//   }
-//   next();
-// };
+exports.checkID = (req, res, next, val) => {
+  //console.log(`Tour id is: ${val}`);
+  if (+req.params.id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  next();
+};
 
 exports.checkBody = (req, res, next) => {
   const { name, price } = req.body;
